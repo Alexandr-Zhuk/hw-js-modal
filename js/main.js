a9bd2300732b30a3bbca636ac313f1cb7b7532b6
Alexand-Zhuk <alexandrzhuk88@gmail.com>
2023-02-07 13:10:07 +0200

    done next hw, add input Name and other funcs

--- js/main.js
+++ js/main.js
@@ -2,37 +2,79 @@ let btnModal = document.querySelector('.open-modal');
 let modalWindow = document.querySelector('.modal');
 let closeModal = document.querySelector('.close-modal');
 let sex = document.querySelector('.sex');
+let nameVal = document.querySelector('.name > input');
+let namePlaceholder = document.querySelector('.name > input[placeholder]');
+let age = document.querySelector('.age');
+let birthDay;
+let res = document.querySelector('.result');
+
 
 sex.addEventListener('change', () => {
-    let age = document.querySelector('.age');
-    let res = document.querySelector('.result')
+    let name = document.querySelector('.name');
+    if(sex.value === ''){
+        name.classList.remove('show');
+        res.innerHTML = 'Выберите пол';
+        return;
+    }
+
+    if(sex.value === 'female'){
+        nameVal.setAttribute('placeholder', 'Зина')
+    }
 
     if(sex.value === 'male'){
+        nameVal.setAttribute('placeholder', 'Иван')
+    }
+
+    res.innerHTML = '';
+    name.classList.add('show');
+    nameVal.value = '';
+    age.classList.remove('show');
+});
+let addName = document.querySelector('.age-add');
+let startVal = addName.innerHTML;
+
+const ucFirst = (string) => {
+    return string[0].toUpperCase() + string.slice(1);
+};
+
+const goodName = (name) => {
+    return ucFirst(name.toLowerCase());
+};
+
+const checkAdult = () =>{
+    let dateBirth = Number(moment(birthDay.value, 'DD.MM.YYYY').format('x'));
+    let isAdultStamp = Number(moment(dateBirth, 'x').add(18, 'years').format('x'));
+    let now = Number(moment().format('x'));
+    if(isAdultStamp < now){
+        res.innerHTML = goodName(nameVal.value) + ', ты уже взрослый';
+    }else{
+        res.innerHTML = goodName(nameVal.value) + ', подрости малеха';
+    }
+};
+
+const checkSex = () => {
+    if(sex.value === 'male' && nameVal.value !== ''){
         age.classList.add('show');
         res.innerHTML = '';
-        let birthDay = document.querySelector('.age > input');
-
-        birthDay.addEventListener('blur', () => {
-            let dateBirth = Number(moment(birthDay.value, 'DD.MM.YYYY').format('x'));
-            let isAdultStamp = Number(moment(dateBirth, 'x').add(18, 'years').format('x'));
-            let now = Number(moment().format('x'));
-            if(isAdultStamp < now){
-                res.innerHTML = 'Ты уже взрослый';
-            }else{
-                res.innerHTML = 'Братан, подрости малеха';
-            }
-        });
-      
         
-
-    }else if(sex.value === 'female'){
+        addName.innerHTML = goodName(nameVal.value) + ', ' + startVal;
+        birthDay = document.querySelector('.age > input');
+        if(birthDay.value !== ''){
+            checkAdult();
+        }
+        birthDay.addEventListener('blur', checkAdult);
+      
+    }else if(sex.value === 'female' && nameVal.value !== ''){
         age.classList.remove('show');
-        res.innerHTML = 'У девушек не спрашивают возраст!'
+
+        res.innerHTML = nameVal.value + ', Вы прекрасны. И у девушек не спрашивают возраст!'
     }else{
         age.classList.remove('show');
-        res.innerHTML = 'Для начала выберите пол';
+        res.innerHTML = 'Введите имя';
     }
-});
+};
+
+nameVal.addEventListener('blur', checkSex);   
 
 
 btnModal.addEventListener('click', () => {
